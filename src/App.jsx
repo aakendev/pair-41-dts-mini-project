60d6c4d01e1a658565ac2ea3859776d31b016aa1
Aldy Rizky Putra <aakendev@gmail.com>
2022-07-21 21:31:18 +0700

    Remove Bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,91 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Navbar from './components/Navbar';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import Categories from './components/Categories';
-import TopRated from './components/TopRated';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Categories from "./components/Categories";
+import TopRated from "./components/TopRated";
 
 const api_url = process.env.REACT_APP_API_IMDB;
 const key = process.env.REACT_APP_KEY_IMDB;
 
 const App = () => {
-    const [main, setMain] = useState([]);
+  const [main, setMain] = useState([]);
 
-    const getMovieById = (data) => {
-        const all_data = [];
-        data.forEach(item => {
-            const apiUrl = api_url + 'movie/' + + item.id +'?api_key='+key;
-            const urlImg = api_url + 'movie/' + + item.id +'/images?api_key='+key+'&language=en-US&include_image_language=en,null';
-            axios.get(apiUrl).then(res => {
-                axios.get(urlImg).then(result => {
-                    all_data.push({
-                        backdrop_path: res.data.backdrop_path,
-                        genres: res.data.genres[0],
-                        title: res.data.title,
-                        tagline: res.data.tagline,
-                        overview: res.data.overview,
-                        id: res.data.id,
-                        logo: result.data.logos[0].file_path
-                    })
-                    setMain(all_data.slice(0,5))
-                })
-            })  
-        })
-    }
+  const getMovieById = (data) => {
+    const all_data = [];
+    data.forEach((item) => {
+      const apiUrl = api_url + "movie/" + +item.id + "?api_key=" + key;
+      const urlImg =
+        api_url +
+        "movie/" +
+        +item.id +
+        "/images?api_key=" +
+        key +
+        "&language=en-US&include_image_language=en,null";
+      axios.get(apiUrl).then((res) => {
+        axios.get(urlImg).then((result) => {
+          all_data.push({
+            backdrop_path: res.data.backdrop_path,
+            genres: res.data.genres[0],
+            title: res.data.title,
+            tagline: res.data.tagline,
+            overview: res.data.overview,
+            id: res.data.id,
+            logo: result.data.logos[0].file_path,
+          });
+          setMain(all_data.slice(0, 5));
+        });
+      });
+    });
+  };
 
-    
-    useEffect(() => {
-        const getAPI = () => {
-            const apiUrl = api_url+'movie/'+'popular?api_key='+key
-            axios.get(apiUrl).then(response => {
-                getMovieById(response.data.results.slice(0,5));
-            })
-        }
-        getAPI()
-    }, [])
+  useEffect(() => {
+    const getAPI = () => {
+      const apiUrl = `${api_url}movie/popular?api_key=${key}`;
+      axios.get(apiUrl).then((response) => {
+        getMovieById(response.data.results.slice(0, 5));
+      });
+    };
+    getAPI();
+  }, []);
 
-    return (
-        <>
-            <Navbar />
-            <Main movie={main} type="movie" />
-            <Categories id="1" getUrl="popular" title="Popular" getImage="back" type="movie" />
-            <Categories id="1" getUrl="popular" title="Tv Series" getImage="back" type="tv" />
-            <Categories id="2" getUrl="upcoming" title="Upcoming" getImage="poster" type="movie" />
-            <Categories id="2" getUrl="on_the_air" title="On Going" getImage="poster" type="tv" />
-            <TopRated id="3" getUrl="top_rated" title="Top Rated" type="movie" />
-            <Footer />
-      </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <Main movie={main} type="movie" />
+      <Categories
+        id="1"
+        getUrl="popular"
+        title="Popular"
+        getImage="back"
+        type="movie"
+      />
+      <Categories
+        id="1"
+        getUrl="popular"
+        title="Tv Series"
+        getImage="back"
+        type="tv"
+      />
+      <Categories
+        id="2"
+        getUrl="upcoming"
+        title="Upcoming"
+        getImage="poster"
+        type="movie"
+      />
+      <Categories
+        id="2"
+        getUrl="on_the_air"
+        title="On Going"
+        getImage="poster"
+        type="tv"
+      />
+      <TopRated id="3" getUrl="top_rated" title="Top Rated" type="movie" />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,76 +1,90 @@
-import React, { useState } from 'react';
-import logo from '../assets/images/logo.png';
-import { Link, useNavigate } from 'react-router-dom';
-import { signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
-import { auth } from '../config/firebase';
-
+import React, { useState } from "react";
+import logo from "../assets/images/logo.png";
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/firebase";
 
 const Login = () => {
   const navigate = useNavigate();
-  const [errorMessage, setErrorMessage] = useState('');
-  
+  const [errorMessage, setErrorMessage] = useState("");
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = new FormData(e.currentTarget);
-    const email = data.get('email');
-    const password = data.get('password');
-    
+    const email = data.get("email");
+    const password = data.get("password");
+
     try {
       await signInWithEmailAndPassword(auth, email, password);
       navigate("/");
     } catch (error) {
       setErrorMessage(error.message);
     }
-  }
-  
-    return (
-        <>
-        <div className='w-full h-screen'>
-            <img
-            className='hidden sm:block absolute w-full h-screen object-cover'
-            src='https://templates.iqonic.design/streamit/frontend/html/images/login/login.jpg'
-            alt='/'
-            />
-            <div className='fixed w-full px-4 py-24 z-50'>
-            <div className='max-w-[450px] h-auto mx-auto bg-black/75 text-white'>
-                <div className='max-w-[320px] mx-auto py-16'>
-                <div className='justify-center'>
-                    <Link to="/"><img src={logo} className="w-full h-20 object-contain" alt="logo" /></Link>
-                    <h1 className='mt-4 mb-4 text-center'>Sign In to your account</h1>
-                </div>
-                <form
-                    onSubmit={handleSubmit}
-                    className='w-full flex flex-col py-4'
-                >
-                    <input
-                    //   onChange={(e) => setEmail(e.target.value)}
-                    className='p-3 my-2 bg-black border focus:border-red-600 focus:ring-red-600 focus:outline-none focus:ring focus:ring-opacity-40'
-                    type='email'
-                    placeholder='Email'
-                    autoComplete='email'
-                    name='email'
-                    />
-                    <input
-                    //   onChange={(e) => setPassword(e.target.value)}
-                    className='p-3 my-2 bg-black border focus:border-red-600 focus:ring-red-600 focus:outline-none focus:ring focus:ring-opacity-40'
-                    type='password'
-                    placeholder='Password'
-                    autoComplete='current-password'
-                    name='password'
-                    />
-                    <p className='text-xs text-red-400 text-center'>{errorMessage}</p>
-                    <button className='bg-red-600 py-3 my-3 font-bold'>
-                    Sign In
-                    </button>
-                    <div className='mt-2 text-xs'>Dont have an account? <Link to="/signup">Sign Up</Link></div>
-                    <div className='mt-2 text-xs'>Forget Something? <Link to="/reset-password">Reset Password</Link></div>
-                </form>
-                </div>
-            </div>
-            </div>
-        </div>
-      </>
-    )
-}
+  };
 
-export default Login
+  return (
+    <>
+      <div className="w-full h-screen">
+        <img
+          className="hidden sm:block absolute w-full h-screen object-cover"
+          src="https://templates.iqonic.design/streamit/frontend/html/images/login/login.jpg"
+          alt="/"
+        />
+        <div className="fixed w-full px-4 py-24 z-50">
+          <div className="max-w-[450px] h-auto mx-auto bg-black/75 text-white">
+            <div className="max-w-[320px] mx-auto py-16">
+              <div className="justify-center">
+                <Link to="/">
+                  <img
+                    src={logo}
+                    className="w-full h-20 object-contain"
+                    alt="logo"
+                  />
+                </Link>
+                <h1 className="mt-4 mb-4 text-center">
+                  Sign In to your account
+                </h1>
+              </div>
+              <form
+                onSubmit={handleSubmit}
+                className="w-full flex flex-col py-4"
+              >
+                <input
+                  //   onChange={(e) => setEmail(e.target.value)}
+                  className="p-3 my-2 bg-black border focus:border-red-600 focus:ring-red-600 focus:outline-none focus:ring focus:ring-opacity-40"
+                  type="email"
+                  placeholder="Email"
+                  autoComplete="email"
+                  name="email"
+                />
+                <input
+                  //   onChange={(e) => setPassword(e.target.value)}
+                  className="p-3 my-2 bg-black border focus:border-red-600 focus:ring-red-600 focus:outline-none focus:ring focus:ring-opacity-40"
+                  type="password"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                  name="password"
+                />
+                <p className="text-xs text-red-400 text-center">
+                  {errorMessage}
+                </p>
+                <button className="bg-red-600 py-3 my-3 font-bold">
+                  Sign In
+                </button>
+                <div className="mt-2 text-xs">
+                  Dont have an account? <Link to="/signup">Sign Up</Link>
+                </div>
+                <div className="mt-2 text-xs">
+                  Forget Something?{" "}
+                  <Link to="/reset-password">Reset Password</Link>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
